d8eafa2b821e1961765545969cd2ebb4b70e51b1
Jae <jaecarbary@gmail.com>
2016-07-01 02:02:03 +0000

    added more user only restrictions to site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def require_user
+    if !user_signed_in?
+    flash[:danger] = "You must be logged in to perform that action."
+    redirect_to root_path
+    end
+  end
+  
 end
